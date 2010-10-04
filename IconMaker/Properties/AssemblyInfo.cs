7c011f33627d8e3db13cefedade28638028bc912
apapadimoulis <apapadimoulis@inedo.com>
2010-10-04 20:33:39 +0000

    Minor updates for release.

--- IconMaker/Properties/AssemblyInfo.cs
+++ IconMaker/Properties/AssemblyInfo.cs
@@ -4,36 +4,20 @@ using System.Resources;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Icon Maker")]
 [assembly: AssemblyDescription("Simple icon creation tool.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Greg Divis")]
+[assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("Icon Maker")]
-[assembly: AssemblyCopyright("Copyright © 2009")]
+[assembly: AssemblyCopyright("Copyright © 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
