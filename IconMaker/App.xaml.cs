1ed22541644ae91ee9c441cab50d525fbce31f06
Eric Lindahl <eric@sciumo.com>
2020-06-22 16:48:12 -0400

    try-convert with stylecop/code analysis
    tested and works.

--- IconMaker/App.xaml.cs
+++ IconMaker/App.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System.Windows;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="Inedo">
+//     Inedo 2011
+// </copyright>
+//-----------------------------------------------------------------------
 namespace IconMaker
 {
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
